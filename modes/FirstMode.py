0e69652d09e14f6aaecd42b6f1a794c0ca504c17
Алмаз Сиразитдинов <asirazitdionv@htc-cs.ru>
2019-12-21 15:32:37 +0400

    refactor(first mode): color delta

--- modes/FirstMode.py
+++ modes/FirstMode.py
@@ -24,15 +24,10 @@ def next_color(c_from, c_to, d_r_col, d_g_col, d_b_col):
     c_r = c_from[0] - d_r_col if c_from[0] > c_to[0] else c_from[0] + d_r_col
     c_g = c_from[1] - d_g_col if c_from[1] > c_to[1] else c_from[1] + d_g_col
     c_b = c_from[2] - d_b_col if c_from[2] > c_to[2] else c_from[2] + d_b_col
-    result = (c_r, c_g, c_b)
 
-    if result == c_to:
-        c_to = c_from
-        c_from = result
+    return (c_r, c_g, c_b)
 
-    return result
-
-def get_color(c_from, c_to, step):
+def get_delta_color(c_from, c_to, step):
     d_r_col = math.floor(c_from[0] - c_to[0]) if c_from[0] > c_to[0] else math.floor(c_to[0] - c_from[0])
     d_r_col = math.floor(d_r_col / step) if d_r_col != 0 else 0
     d_g_col = math.floor(c_from[1] - c_to[1]) if c_from[1] > c_to[1] else math.floor(c_to[1] - c_from[1])
@@ -40,9 +35,13 @@ def get_color(c_from, c_to, step):
     d_b_col = math.floor(c_from[2] - c_to[2]) if c_from[2] > c_to[2] else math.floor(c_to[2] - c_from[2])
     d_b_col = math.floor(d_b_col / step) if d_b_col != 0 else 0
 
-    previous_color = next_color(c_from, c_to, d_r_col, d_g_col, d_b_col)
+    return (d_r_col, d_g_col, d_b_col)
+
+def get_color(c_from, c_to, step):
+    delta_color = get_delta_color(c_from, c_to, step)
+    previous_color = next_color(c_from, c_to, delta_color[0], delta_color[1], delta_color[2])
     while True:
-        previous_color = next_color(previous_color, c_to, d_r_col, d_g_col, d_b_col)
+        previous_color = next_color(previous_color, c_to, delta_color[0], delta_color[1], delta_color[2])
         yield previous_color
 
 canvas = cv.Canvas()
@@ -61,4 +60,4 @@ while True:
             xItem[1] = color[1]
             xItem[2] = color[2]
     canvas.draw(array)
-    time.sleep(0.3)
+    time.sleep(0.1)
