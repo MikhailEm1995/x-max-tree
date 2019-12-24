b9c6c67d14ab13da30e97211b4bfba0b898cad46
Алмаз Сиразитдинов <asirazitdionv@htc-cs.ru>
2019-12-24 22:57:19 +0400

    refactor(*): enhanced a drawing performance

--- services/Canvas.py
+++ services/Canvas.py
@@ -2,13 +2,17 @@ from graphics import *
 
 
 class Canvas:
+    rects = []
+    rects_delete = []
+
     def __init__(self):
         self.win_width = 800
         self.win_height = 600
-        self.window = GraphWin("Testing modes", self.win_width, self.win_height)
+        self.window = GraphWin("Testing modes", self.win_width, self.win_height, False)
 
     def draw(self, array):
-        self.clear()
+        self.rects_delete = self.rects
+        self.rects = []
         width, height = 30, 30
         start_point = Point(self.win_width / 2 - (width * len(array[0])) / 2, self.win_height / 2 - (height * len(array)) / 2)
         current_point = Point(start_point.x, start_point.y)
@@ -21,10 +25,12 @@ class Canvas:
                 rect = Rectangle(current_point, Point(current_point.x + width, current_point.y + height))
                 rect.setFill(color_rgb(xItem[0], xItem[1], xItem[2]))
                 rect.draw(self.window)
+                self.rects.append(rect)
                 current_point = Point(current_point.x + width, current_point.y)
             current_point = Point(start_point.x, current_point.y + height)
+        self.clear_rects()
 
-    def clear(self):
-        for item in self.window.items[:]:
+    def clear_rects(self):
+        for item in self.rects_delete[:]:
             item.undraw()
         self.window.update()
