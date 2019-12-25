d57496a9e701e2dc42b7751e4a1970424fa9b37f
Osipov Evgeny <eosipov@htc-cs.ru>
2019-12-25 21:32:25 +0400

    feat: added draw mode

--- modes/DrawMode.py
+++ modes/DrawMode.py
@@ -1,48 +1,78 @@
+import math
 import numpy as np
 import cv2
 
-# import services.Color as Color
-# import services.Canvas as cv
-# import time
-#
-# array = [
-#     [None, None, None, None, [0, 0, 0], None, None, None, None],
-#     [None, None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None, None],
-#     [None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None],
-#     [None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None],
-#     [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
-#     [None, None, None, None, [0, 0, 0], None, None, None, None],
-#     [None, None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None, None],
-#     [None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None],
-#     [None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None],
-#     [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
-#     [None, None, None, None, [0, 0, 0], None, None, None, None],
-#     [None, None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None, None],
-#     [None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None],
-#     [None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None],
-#     [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
-# ]
+paintColor = (255, 0, 0)
+
+camera = {
+    'width': 640,
+    'height': 320,
+}
+
+array = [
+    [None, None, None, None, [0, 0, 0], None, None, None, None],
+    [None, None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None, None],
+    [None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None],
+    [None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [None, None, None, None, [0, 0, 0], None, None, None, None],
+    [None, None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None, None],
+    [None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None],
+    [None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [None, None, None, None, [0, 0, 0], None, None, None, None],
+    [None, None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None, None],
+    [None, None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None, None],
+    [None, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], None],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+]
+
+arrayWidth = len(array[0])
+arrayHeight = len(array)
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+# задаем границы ядовито зеленого цвета.
+# Изначально цвет задается в HSV-формате, который маппится по правилам:
+# 0º-359º <-> [0-179]
+# 0%-100% <-> [0-255]
+# последний параметр, value, пробрасывается без маппинга
+lowerBound = np.array([80, 100, 255])
+upperBound = np.array([120, 255, 255])
+
+
+def getXs(x):
+    return x * arrayWidth / camera['width']
+
+
+def getYs(y):
+    return y * arrayHeight / camera['height']
+
+
+def getPointInArray(point):
+    return math.floor(getXs(point[0])), math.floor(getYs(point[1]))
+
+
+def setColorInArray(point):
+    x = point[0]
+    y = point[1]
+    if array[y][x] is not None:
+        array[y][x] = paintColor
+        print(array)
+
+while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    img = cv2.resize(img, (340, 220))
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lowerBound = np.array([80, 100, 255])
-    upperBound = np.array([120, 255, 255])
-    mask = cv2.inRange(hsv_frame, lowerBound, upperBound)  # поиск ядовито зеленого цвета.
+    img = cv2.resize(frame, (camera['width'], camera['height']))
+    hsv_frame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # Display the resulting frame
-    cv2.imshow('frame', mask)
+    mask = cv2.inRange(hsv_frame, lowerBound, upperBound)
+
     points = cv2.findNonZero(mask)
     if points is not None:
-        avg = np.mean(points, axis=0)
-        print(avg)
+        averagePoint = np.mean(points, axis=0)[0]
+        pointInArray = getPointInArray(averagePoint)  # получаем точку в ёлке
+        setColorInArray(pointInArray)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
