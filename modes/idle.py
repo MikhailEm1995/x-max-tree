f85fa2e79800a501b7d6f87565900ebade077743
Сиразитдинов Алмаз <asirazitdinov@htc-cs.ru>
2019-12-25 15:53:12 +0400

    feat(idle): random mode

--- modes/idle.py
+++ modes/idle.py
@@ -1,4 +1,5 @@
 import services.Color as Color
+import random
 import time
 
 
@@ -10,8 +11,14 @@ import time
 # config.current_color = (number, number, number)
 # config.play_sound = def func(local_sound_filename):
 
-
 def idle(config):
+    mode = random_mode(config)
+
+    while True:
+        yield next(mode)
+
+
+def flow_mode(config):
     color_current = Color.animate_array_colors([
         (0, 57, 56),  # dark green
         (0, 134, 91),  # light green
@@ -27,6 +34,19 @@ def idle(config):
         for y in range(config["matrix_height"]):
             array.append([])
             for x in range(config["matrix_width"]):
+                array[y].append(color)
+        time.sleep(0.1)
+        yield array
+
+
+def random_mode(config):
+    while True:
+        array = []
+
+        for y in range(config["matrix_height"]):
+            array.append([])
+            for x in range(config["matrix_width"]):
+                color = (random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1))
                 array[y].append(color)
         time.sleep(0.1)
         yield array
